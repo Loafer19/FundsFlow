b683ed6701fdd01c562ebcdefeaceedf35c2870b
Popelyshyn Ruslan <platonpopelishin@gmail.com>
2025-04-18 17:29:51 +0300

    docker:
    - backend
    - frontend

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AlwaysJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(AlwaysJson::class);
+
         $middleware
             ->statefulApi()
             ->throttleApi('60,1');
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
 
     'allowed_origins_patterns' => [],
 
